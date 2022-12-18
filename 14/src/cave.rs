c1d8919124315c4591c2a8e62a986002748a6cce
Marcel Heistermann <mheistermann@gmx.de>
2022-12-18 18:15:47 +0100

    Part 1

--- 14/src/cave.rs
+++ 14/src/cave.rs
@@ -3,28 +3,31 @@ use std::str::FromStr;
 use anyhow::{anyhow, Context};
 use common::twod::{Grid, Point};
 
+const SAND_SOURCE_X: i32 = 500;
+
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub enum Cell {
+pub enum Tile {
     Air,
     Rock,
     Sand,
     SandSource,
 }
 
-impl From<Cell> for char {
-    fn from(value: Cell) -> Self {
+impl From<Tile> for char {
+    fn from(value: Tile) -> Self {
         match value {
-            Cell::Air => '.',
-            Cell::Rock => '#',
-            Cell::Sand => 'o',
-            Cell::SandSource => '+',
+            Tile::Air => '.',
+            Tile::Rock => '#',
+            Tile::Sand => 'o',
+            Tile::SandSource => '+',
         }
     }
 }
 
 pub struct Cave {
-    grid: Grid<Cell>,
+    grid: Grid<Tile>,
     x_offset: i32,
+    y_max: i32, // below y_max, sand falls into the void
 }
 
 impl FromStr for Cave {
@@ -59,16 +62,23 @@ impl FromStr for Cave {
         let width = (x_max - x_min + 1) as usize;
         let grid_size = width * (y_max + 1) as usize;
         let mut grid = Grid {
-            data: vec![Cell::Air; grid_size],
+            data: vec![Tile::Air; grid_size],
             width,
         };
         for path in paths.iter() {
             let mut offset_path = path.clone();
             offset_path.iter_mut().for_each(|p| p.x -= x_offset);
-            grid.fill_path(&offset_path, Cell::Rock);
+            grid.fill_path(&offset_path, Tile::Rock);
         }
-        grid[Point { x: 500 - x_offset, y: 0 }] = Cell::SandSource;
-        Ok(Cave { grid, x_offset })
+        grid[Point {
+            x: SAND_SOURCE_X - x_offset,
+            y: 0,
+        }] = Tile::SandSource;
+        Ok(Cave {
+            grid,
+            x_offset,
+            y_max,
+        })
     }
 }
 
@@ -87,18 +97,73 @@ impl ToString for Cave {
     }
 }
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum DropSandResult {
+    Overflow,
+    SandRests,
+}
+
+impl Cave {
+    fn drop_sand(&mut self, p: Point) -> DropSandResult {
+        let mut p = p;
+        if self.grid[p] == Tile::Sand {
+            return DropSandResult::Overflow;
+        }
+        let mut falling = true;
+        while falling {
+            falling = false;
+            for dest in [
+                Point { x: p.x, y: p.y + 1 },
+                Point {
+                    x: p.x - 1,
+                    y: p.y + 1,
+                },
+                Point {
+                    x: p.x + 1,
+                    y: p.y + 1,
+                },
+            ] {
+                if dest.x < 0 || dest.x >= self.grid.width as i32 || dest.y > self.y_max {
+                    return DropSandResult::Overflow;
+                } else if self.grid[dest] == Tile::Air {
+                    p = dest;
+                    falling = true;
+                    break;
+                }
+            }
+        }
+        self.grid[p] = Tile::Sand;
+        return DropSandResult::SandRests;
+    }
+
+    pub fn fill_sand(&mut self) {
+        let spawn_point = Point {
+            x: SAND_SOURCE_X - self.x_offset,
+            y: 1,
+        };
+        loop {
+            let res = self.drop_sand(spawn_point);
+            if res == DropSandResult::Overflow {
+                return;
+            }
+        }
+    }
+
+    pub fn count_sand(&self) -> usize {
+        self.grid.data.iter().cloned().filter(|&tile| tile == Tile::Sand).count()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn sample_cave() {
+    fn build_sample_cave() {
         let cave: Cave = "498,4 -> 498,6 -> 496,6\n\
                           503,4 -> 502,4 -> 502,9 -> 494,9"
             .parse()
             .unwrap();
-        println!("{}", cave.x_offset);
-        println!("{}", cave.to_string());
         assert_eq!(
             cave.to_string(),
             "0000 ......+...\n\
@@ -112,5 +177,29 @@ mod tests {
              0008 ........#.\n\
              0009 #########."
         );
+        assert_eq!(cave.count_sand(), 0);
+    }
+
+    #[test]
+    fn fill_sample_cave() {
+        let mut cave: Cave = "498,4 -> 498,6 -> 496,6\n\
+                              503,4 -> 502,4 -> 502,9 -> 494,9"
+            .parse()
+            .unwrap();
+        cave.fill_sand();
+        assert_eq!(
+            cave.to_string(),
+            "0000 ......+...\n\
+             0001 ..........\n\
+             0002 ......o...\n\
+             0003 .....ooo..\n\
+             0004 ....#ooo##\n\
+             0005 ...o#ooo#.\n\
+             0006 ..###ooo#.\n\
+             0007 ....oooo#.\n\
+             0008 .o.ooooo#.\n\
+             0009 #########."
+        );
+        assert_eq!(cave.count_sand(), 24);
     }
 }
--- 14/src/main.rs
+++ 14/src/main.rs
@@ -1,5 +1,14 @@
 mod cave;
 
+use std::{env, fs};
+use cave::Cave;
+
 fn main() {
-    println!("Hello, world!");
+    let input_file_path = env::args().nth(1).unwrap_or("14/input.txt".into());
+    let input = fs::read_to_string(&input_file_path).unwrap();
+    let mut cave :Cave= input.parse().unwrap();
+    cave.fill_sand();
+    let sand_count = cave.count_sand();
+    // println!("{}", cave.to_string());
+    println!("{sand_count} units of sand have come to rest");
 }
