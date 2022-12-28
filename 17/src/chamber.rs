5fdd50ba46d09f0f6f01d64ac8d7bc7b95b04e8c
Marcel Heistermann <mheistermann@gmx.de>
2022-12-28 01:19:43 +0100

    optimize with bitfields; still too slow

--- 17/src/chamber.rs
+++ 17/src/chamber.rs
@@ -1,14 +1,14 @@
+use common::twod::Point;
 use std::{fmt, str::FromStr};
 
-use common::twod::{Grid, Point};
+const CHAMBER_WIDTH: usize = 7;
+const MAX_ROCK_SHAPE_HEIGHT: usize = 4;
+// Init value puts a 'wall' at the right hand side for collision detection
+const GRID_ROW_INITVAL: u8 = 1 << 7;
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
-enum Tile {
-    Air,
-    Rock,
-}
-
-type RockShape = Grid<Tile>;
+/// Rock shapes as bit mask (one u8 per line), in different horizontal positions (shape[2]
+/// would be the initial position)
+type RockShape = [[u8; MAX_ROCK_SHAPE_HEIGHT]; CHAMBER_WIDTH + 1];
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum Jet {
@@ -20,7 +20,7 @@ pub enum Jet {
 pub struct JetPattern(Vec<Jet>);
 
 pub struct Chamber {
-    grid: Grid<Tile>,
+    grid: Vec<u8>,
     base_y: usize, // height from which we store data in grid
     high_point: i32,
     rock_shapes: Vec<RockShape>,
@@ -33,10 +33,7 @@ impl Chamber {
     pub fn new(jet_pattern: JetPattern) -> Self {
         let rock_shapes = get_rock_shapes();
         Self {
-            grid: Grid {
-                data: vec![Tile::Air; 7 * 5000],
-                width: 7,
-            },
+            grid: vec![GRID_ROW_INITVAL; 5000],
             base_y: 0,
             rock_shapes,
             high_point: -1,
@@ -51,8 +48,11 @@ impl Chamber {
     }
 
     pub fn rumble(&mut self, n_rocks: usize) {
-        for _ in 0..n_rocks {
+        for i in 0..n_rocks {
             self.drop_rock();
+            if i % 100_000_000 == 0 {
+                println!("(INFO) {}M rocks dropped", i / 1_000_000);
+            }
         }
     }
 
@@ -63,8 +63,10 @@ impl Chamber {
             .unwrap()
             .clone();
         self.next_rock_shape_idx = (self.next_rock_shape_idx + 1) % self.rock_shapes.len();
-        self.grid
-            .ensure_height(self.tower_height() + 4 + rock.height(), Tile::Air);
+        let req_height = self.tower_height() + 4 + MAX_ROCK_SHAPE_HEIGHT;
+        if self.grid.len() < req_height {
+            self.grid.resize(req_height, GRID_ROW_INITVAL);
+        }
         let mut anchor = Point {
             x: 2,
             y: self.high_point + 4,
@@ -95,48 +97,31 @@ impl Chamber {
                 anchor = anchor_fallen;
             }
         }
-        self.come_to_rest(rock, anchor);
+        self.come_to_rest(&rock, anchor);
     }
 
-    fn collision(&self, rock: &Grid<Tile>, anchor: Point) -> bool {
-        for y in 0..rock.height() as i32 {
-            for x in 0..rock.width() as i32 {
-                if rock[Point { x, y }] == Tile::Air {
-                    continue;
-                }
-                let grid_point = Point {
-                    x: x + anchor.x,
-                    y: y + anchor.y,
-                };
-                if grid_point.x < 0
-                    || grid_point.x >= self.grid.width as i32
-                    || grid_point.y < 0
-                    || self.grid[grid_point] != Tile::Air
-                {
-                    return true;
-                }
-            }
+    fn collision(&self, rock: &RockShape, anchor: Point) -> bool {
+        if anchor.x < 0 || anchor.y < 0 {
+            return true;
         }
-        false
+        let rock = rock[anchor.x as usize]; // x-translated shape
+        self.grid[anchor.y as usize..anchor.y as usize + MAX_ROCK_SHAPE_HEIGHT]
+            .iter()
+            .zip(rock.iter())
+            .map(|(&a, &b)| a & b)
+            .any(|r| r != 0)
     }
 
-    fn come_to_rest(&mut self, rock: Grid<Tile>, anchor: Point) {
-        for y in 0..rock.height() as i32 {
-            for x in 0..rock.width() as i32 {
-                if rock[Point { x, y }] == Tile::Air {
-                    continue;
-                }
-                let grid_point = Point {
-                    x: x + anchor.x,
-                    y: y + anchor.y,
-                };
-                let grid_tile = &mut self.grid[grid_point];
-                if *grid_tile == Tile::Air {
-                    *grid_tile = Tile::Rock;
-                    self.high_point = std::cmp::max(self.high_point, grid_point.y);
-                }
-            }
-        }
+    fn come_to_rest(&mut self, rock: &RockShape, anchor: Point) {
+        let rock = rock[anchor.x as usize]; // x-translated shape
+        self.grid[anchor.y as usize..anchor.y as usize + MAX_ROCK_SHAPE_HEIGHT]
+            .iter_mut()
+            .zip(rock.iter())
+            .for_each(|(a, &b)| *a |= b);
+        self.high_point = (std::cmp::max(0, self.high_point)..self.grid.len() as i32)
+            .find(|&v| self.grid[v as usize] & 0b1111111 == 0)
+            .unwrap_or(self.grid.len() as i32)
+            - 1;
         self.adjust_view();
     }
 
@@ -147,18 +132,13 @@ impl Chamber {
         }
         let rows_to_keep = self
             .grid
-            .data
-            .chunks(self.grid.width)
+            .iter()
             .rev()
             .enumerate()
-            .skip(self.grid.height() - (self.high_point + 1) as usize)
-            .scan(vec![false; self.grid.width], |acc, (n, row)| {
-                for (acc, tile) in acc.iter_mut().zip(row.iter()) {
-                    if *tile == Tile::Rock {
-                        *acc = true;
-                    }
-                }
-                if acc.iter().all(|&b| b) {
+            .skip(self.grid.len() - (self.high_point + 1) as usize)
+            .scan(0u8, |acc, (n, row)| {
+                *acc |= *row;
+                if *acc == 0b11111111 {
                     None
                 } else {
                     Some(n)
@@ -167,13 +147,13 @@ impl Chamber {
             .last()
             .unwrap_or(0)
             + 10;
-        if rows_to_keep >= self.grid.height() {
+        if rows_to_keep >= self.grid.len() {
             return;
         }
-        let rows_to_cut = self.grid.height() - rows_to_keep;
+        let rows_to_cut = self.grid.len() - rows_to_keep;
         self.base_y += rows_to_cut;
         self.high_point -= rows_to_cut as i32;
-        self.grid.data.drain(0..rows_to_cut * self.grid.width);
+        self.grid.drain(0..rows_to_cut);
     }
 }
 
@@ -181,15 +161,15 @@ impl fmt::Display for Chamber {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let height = (self.high_point + 2) as usize;
         self.grid
-            .data
-            .chunks(self.grid.width)
+            .iter()
             .take(height)
             .rev()
-            .map(|ln| {
-                ln.iter()
-                    .map(|tile| match tile {
-                        Tile::Air => '.',
-                        Tile::Rock => '#',
+            .map(|&row| {
+                (0..CHAMBER_WIDTH)
+                    .map(|bit_idx| row & (1u8 << bit_idx) != 0)
+                    .map(|rock| match rock {
+                        false => '.',
+                        true => '#',
                     })
                     .map(|ch| write!(f, "{ch}"))
                     .collect::<fmt::Result>()
@@ -199,37 +179,75 @@ impl fmt::Display for Chamber {
     }
 }
 
-fn get_rock_shapes() -> Vec<RockShape> {
-    const INP: &str = "####\n\
-                       \n\
-                       .#.\n\
-                       ###\n\
-                       .#.\n\
-                       \n\
-                       ..#\n\
-                       ..#\n\
-                       ###\n\
-                       \n\
-                       #\n\
-                       #\n\
-                       #\n\
-                       #\n\
-                       \n\
-                       ##\n\
-                       ##";
-    INP.split("\n\n")
-        .map(|shape_str| {
-            let width = shape_str.lines().next().unwrap().len();
-            let data = shape_str
-                .lines()
-                .rev()
-                .flat_map(|ln| ln.chars())
-                .map(|c| match c {
-                    '#' => Tile::Rock,
-                    _ => Tile::Air,
+fn to_string(s: RockShape) -> String {
+    let mut result = "".to_owned();
+    for row in (0..MAX_ROCK_SHAPE_HEIGHT).rev() {
+        for shape in s {
+            result += &(0..CHAMBER_WIDTH)
+                .map(|bit_idx| shape[row] & (1u8 << bit_idx) != 0)
+                .map(|rock| match rock {
+                    false => '.',
+                    true => '#',
                 })
-                .collect();
-            RockShape { width, data }
+                .collect::<String>();
+            result += "  |  ";
+        }
+        result += "\n";
+    }
+    result
+}
+
+#[allow(dead_code)]
+pub fn print_shapes() {
+    let shapes = get_rock_shapes();
+    for sh in shapes {
+        println!("{}\n\n", to_string(sh));
+    }
+}
+
+fn get_rock_shapes() -> Vec<RockShape> {
+    let base_shapes = vec![
+        [
+            0b00001111u8, //
+            0b00000000u8, //
+            0b00000000u8, //
+            0b00000000u8, //
+        ],
+        [
+            0b00000010u8, //
+            0b00000111u8, //
+            0b00000010u8, //
+            0b00000000u8, //
+        ],
+        [
+            0b00000111u8, //
+            0b00000100u8, //
+            0b00000100u8, //
+            0b00000000u8, //
+        ],
+        [
+            0b00000001u8, //
+            0b00000001u8, //
+            0b00000001u8, //
+            0b00000001u8, //
+        ],
+        [
+            0b00000011u8, //
+            0b00000011u8, //
+            0b00000000u8, //
+            0b00000000u8, //
+        ],
+    ];
+    base_shapes
+        .iter()
+        .map(|base| {
+            let mut translation_set = [*base; CHAMBER_WIDTH + 1];
+            for t in 1..CHAMBER_WIDTH + 1 {
+                    translation_set[t].iter_mut().for_each(|row| {
+                        *row <<= t;
+                    })
+            }
+            translation_set
         })
         .collect()
 }
