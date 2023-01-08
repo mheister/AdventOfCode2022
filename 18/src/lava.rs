11f68b9a5806c71afefc039cc1f48fa490d3fc34
Marcel Heistermann <mheistermann@gmx.de>
2023-01-08 14:08:18 +0100

    Day 18 part 1

---
+++ 18/src/lava.rs
@@ -0,0 +1,133 @@
+use std::{collections::HashSet, str::FromStr};
+
+use anyhow::anyhow;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub struct Cube {
+    pub x: i32,
+    pub y: i32,
+    pub z: i32,
+}
+
+impl Cube {
+    pub fn neighbours(&self) -> CubeNeighbours {
+        CubeNeighbours { cube: *self, i: 0 }
+    }
+}
+
+pub struct CubeNeighbours {
+    cube: Cube,
+    i: u8,
+}
+
+impl Iterator for CubeNeighbours {
+    type Item = Cube;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.i >= 6 {
+            return None;
+        }
+        let mut result = self.cube;
+        let coord =
+            &mut [&mut result.x, &mut result.y, &mut result.z][(self.i % 3) as usize];
+        let off = [-1, 1][(self.i / 3) as usize];
+        **coord += off;
+        self.i += 1;
+        Some(result)
+    }
+}
+
+pub struct Droplet(Vec<Cube>);
+
+impl FromStr for Droplet {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Droplet(
+            s.lines()
+                .map(|ln| -> Result<Cube, Self::Err> {
+                    let (x, yz) = ln.split_once(',').ok_or(anyhow!("Expected ','"))?;
+                    let (y, z) = yz.split_once(',').ok_or(anyhow!("Expected ','"))?;
+                    Ok(Cube {
+                        x: x.parse()?,
+                        y: y.parse()?,
+                        z: z.parse()?,
+                    })
+                })
+                .collect::<Result<Vec<Cube>, _>>()?,
+        ))
+    }
+}
+
+impl Droplet {
+    pub fn surface_area(&self) -> usize {
+        let cubes_map = self.0.iter().collect::<HashSet<_>>();
+        let mut surface = 0;
+        for cube in self.0.iter() {
+            surface += 6;
+            for n in cube.neighbours() {
+                if cubes_map.contains(&n) {
+                    surface -= 1;
+                }
+            }
+        }
+        surface
+    }
+
+    fn is_outside(cube: &Cube) -> bool {
+        // All coordinates are between 0 and 20, non-inclusive
+        const MIN: i32 = 0;
+        const MAX: i32 = 20;
+        cube.x == MIN
+            || cube.x >= MAX
+            || cube.y == MIN
+            || cube.y >= MAX
+            || cube.z == MIN
+            || cube.z >= MAX
+    }
+
+    pub fn exterior_surface_area(&self) -> usize {
+        let cubes_map = self.0.iter().collect::<HashSet<_>>();
+        let mut air_cubes = HashSet::<Cube>::new();
+        let mut surface = 0;
+        for cube in self.0.iter() {
+            surface += 6;
+            for n in cube.neighbours() {
+                if cubes_map.contains(&n) {
+                    surface -= 1;
+                } else {
+                    air_cubes.insert(n);
+                }
+            }
+        }
+        while !air_cubes.is_empty() {
+            let starting_air_cube = air_cubes.iter().next().unwrap().clone();
+            air_cubes.remove(&starting_air_cube);
+            let mut air_pocket = vec![starting_air_cube];
+            let mut pidx = 0;
+            loop {
+                if pidx >= air_pocket.len() {
+                    surface -= Droplet(air_pocket).surface_area();
+                    break;
+                }
+                let mut outside = false;
+                for n in air_pocket.get(pidx).unwrap().neighbours() {
+                    air_cubes.remove(&n);
+                    if !cubes_map.contains(&n) {
+                        if Droplet::is_outside(&n) {
+                            // No air pocket
+                            outside = true;
+                            break;
+                        }
+                        air_pocket.push(n);
+                    }
+                }
+                if outside {
+                    break;
+                }
+                pidx += 1;
+            }
+        }
+        surface
+    }
+}
--- 18/src/main.rs
+++ 18/src/main.rs
@@ -1,53 +1,70 @@
-use std::str::FromStr;
+use std::{env, fs};
 
-use anyhow::anyhow;
-
-struct Cube {
-    x: i32,
-    y: i32,
-    z: i32,
-}
-
-struct Droplet(Vec<Cube>);
-
-impl FromStr for Droplet {
-    type Err = anyhow::Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Droplet(
-            s.lines()
-                .map(|ln| -> Result<Cube, Self::Err> {
-                    let (x, yz) = ln.split_once(',').ok_or(anyhow!("Expected ','"))?;
-                    let (y, z) = yz.split_once(',').ok_or(anyhow!("Expected ','"))?;
-                    Ok(Cube {
-                        x: x.parse()?,
-                        y: y.parse()?,
-                        z: z.parse()?,
-                    })
-                })
-                .collect::<Result<Vec<Cube>, _>>()?,
-        ))
-    }
-}
-
-impl Droplet {
-    fn count_sides(&self) -> usize {
-        0
-    }
-}
+mod lava;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
+    fn single_cube_has_surface_of_6() {
+        let drop: lava::Droplet = "1,1,1".parse().unwrap();
+        assert_eq!(drop.surface_area(), 6);
+    }
+
+    #[test]
     fn simple_example() {
-        let drop: Droplet = "1,1,1\n2,1,1".parse().unwrap();
-        assert_eq!(drop.count_sides(), 10);
+        let drop: lava::Droplet = "1,1,1\n2,1,1".parse().unwrap();
+        assert_eq!(drop.surface_area(), 10);
+    }
+
+    #[test]
+    fn larger_example() {
+        let drop: lava::Droplet = "2,2,2\n\
+                                   1,2,2\n\
+                                   3,2,2\n\
+                                   2,1,2\n\
+                                   2,3,2\n\
+                                   2,2,1\n\
+                                   2,2,3\n\
+                                   2,2,4\n\
+                                   2,2,6\n\
+                                   1,2,5\n\
+                                   3,2,5\n\
+                                   2,1,5\n\
+                                   2,3,5"
+            .parse()
+            .unwrap();
+        assert_eq!(drop.surface_area(), 64);
+    }
+
+    #[test]
+    fn exterior_surface_larger_example() {
+        let drop: lava::Droplet = "2,2,2\n\
+                                   1,2,2\n\
+                                   3,2,2\n\
+                                   2,1,2\n\
+                                   2,3,2\n\
+                                   2,2,1\n\
+                                   2,2,3\n\
+                                   2,2,4\n\
+                                   2,2,6\n\
+                                   1,2,5\n\
+                                   3,2,5\n\
+                                   2,1,5\n\
+                                   2,3,5"
+            .parse()
+            .unwrap();
+        assert_eq!(drop.exterior_surface_area(), 58);
     }
 }
 
-
 fn main() {
-    println!("Hello, world!");
+    let input_file_path = env::args().nth(1).unwrap_or("18/input.txt".into());
+    let input = fs::read_to_string(&input_file_path).unwrap();
+    let drop: lava::Droplet = input.parse().unwrap();
+    let surface = drop.surface_area();
+    println!("Estimated surface area of lava droplet: {surface}");
+    let exterior_surface = drop.exterior_surface_area();
+    println!("Estimated exterior surface area of lava droplet: {exterior_surface}");
 }
